662b9e73f9fd00db5ccedc1ec9fd21fd3085feac
hhsqdw <hhsqdmz@gmail.com>
2025-06-05 10:52:26 +0800

    新增方法司机抢单robNewOrder以及对之前乘客下单方法saveOrderInfo的修改

--- service/service-order/src/main/java/com/atguigu/daijia/order/service/impl/OrderInfoServiceImpl.java
+++ service/service-order/src/main/java/com/atguigu/daijia/order/service/impl/OrderInfoServiceImpl.java
@@ -1,5 +1,8 @@
 package com.atguigu.daijia.order.service.impl;
 
+import com.atguigu.daijia.common.constant.RedisConstant;
+import com.atguigu.daijia.common.execption.GuiguException;
+import com.atguigu.daijia.common.result.ResultCodeEnum;
 import com.atguigu.daijia.model.entity.order.OrderInfo;
 import com.atguigu.daijia.model.entity.order.OrderStatusLog;
 import com.atguigu.daijia.model.enums.OrderStatus;
@@ -11,10 +14,12 @@ import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 @Service
 @SuppressWarnings({"unchecked", "rawtypes"})
@@ -26,20 +31,29 @@ public class OrderInfoServiceImpl extends ServiceImpl<OrderInfoMapper, OrderInfo
     @Autowired
     private OrderStatusLogMapper orderStatusLogMapper;
 
-    // 乘客下单
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    //乘客下单
     @Override
     public Long saveOrderInfo(OrderInfoForm orderInfoForm) {
-        // 向order_info表中插入数据
+        //order_info添加订单数据
         OrderInfo orderInfo = new OrderInfo();
-        BeanUtils.copyProperties(orderInfoForm, orderInfo);
-
-        orderInfo.setOrderNo(UUID.randomUUID().toString().replaceAll("-", ""));
+        BeanUtils.copyProperties(orderInfoForm,orderInfo);
+        //订单号
+        String orderNo = UUID.randomUUID().toString().replaceAll("-","");
+        orderInfo.setOrderNo(orderNo);
+        //订单状态
         orderInfo.setStatus(OrderStatus.WAITING_ACCEPT.getStatus());
-
         orderInfoMapper.insert(orderInfo);
 
-        // 记录日志
-        this.log(orderInfo.getId(), orderInfo.getStatus());
+        //记录日志
+        this.log(orderInfo.getId(),orderInfo.getStatus());
+
+        //向redis添加标识
+        //接单标识，标识不存在了说明不在等待接单状态了
+        redisTemplate.opsForValue().set(RedisConstant.ORDER_ACCEPT_MARK,
+                "0", RedisConstant.ORDER_ACCEPT_MARK_EXPIRES_TIME, TimeUnit.MINUTES);
 
         return orderInfo.getId();
     }
@@ -58,6 +72,37 @@ public class OrderInfoServiceImpl extends ServiceImpl<OrderInfoMapper, OrderInfo
             return OrderStatus.NULL_ORDER.getStatus();
         }
         return orderInfo.getStatus();
+    }
+
+    //司机抢单
+    @Override
+    public Boolean robNewOrder(Long driverId, Long orderId) {
+        //判断订单是否存在，通过Redis，减少数据库压力
+        if(!redisTemplate.hasKey(RedisConstant.ORDER_ACCEPT_MARK)) {
+            //抢单失败
+            throw new GuiguException(ResultCodeEnum.COB_NEW_ORDER_FAIL);
+        }
+
+        //司机抢单
+        //修改order_info表订单状态值2：已经接单 + 司机id + 司机接单时间
+        //修改条件：根据订单id
+        LambdaQueryWrapper<OrderInfo> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(OrderInfo::getId,orderId);
+        OrderInfo orderInfo = orderInfoMapper.selectOne(wrapper);
+        //设置
+        orderInfo.setStatus(OrderStatus.ACCEPTED.getStatus());
+        orderInfo.setDriverId(driverId);
+        orderInfo.setAcceptTime(new Date());
+        //调用方法修改
+        int rows = orderInfoMapper.updateById(orderInfo);
+        if(rows != 1) {
+            //抢单失败
+            throw new GuiguException(ResultCodeEnum.COB_NEW_ORDER_FAIL);
+        }
+
+        //删除抢单标识
+        redisTemplate.delete(RedisConstant.ORDER_ACCEPT_MARK);
+        return true;
     }
 
     public void log(Long orderId, Integer status) {
