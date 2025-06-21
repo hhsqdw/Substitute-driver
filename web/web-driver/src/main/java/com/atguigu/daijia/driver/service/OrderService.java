7ff21afe47c64ecc60d9820d8893c20bb9818d6e
hhsqdw <hhsqdmz@gmail.com>
2025-06-22 02:35:20 +0800

    完成订单执行三中结束服务接口

--- web/web-driver/src/main/java/com/atguigu/daijia/driver/service/OrderService.java
+++ web/web-driver/src/main/java/com/atguigu/daijia/driver/service/OrderService.java
@@ -1,6 +1,7 @@
 package com.atguigu.daijia.driver.service;
 
 import com.atguigu.daijia.model.form.map.CalculateDrivingLineForm;
+import com.atguigu.daijia.model.form.order.OrderFeeForm;
 import com.atguigu.daijia.model.form.order.StartDriveForm;
 import com.atguigu.daijia.model.form.order.UpdateOrderCartForm;
 import com.atguigu.daijia.model.vo.map.DrivingLineVo;
@@ -37,4 +38,7 @@ public interface OrderService {
 
     // 开始代驾服务
     Boolean startDrive(StartDriveForm startDriveForm);
+
+    // 结束代驾服务更新订单账单
+    Boolean endDrive(OrderFeeForm orderFeeForm);
 }
